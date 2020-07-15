495a477d7934bfdb63405f14229a9c10043aada9
Tony Bowden <tony@tmtm.com>
2020-07-15 07:27:20 +0100

    Deal with explicit '' arguments
    If a string from the addP39s script gets piped into this, but has a
    'blank' argument, that will come as:
    
      add_full_P39.js Q936913 1707-05-01 1709-02-03 '' Q335636
    
    But the 'replaces' here will be a string of two quote-marks, not an
    empty argument, so we need to explicitly check for that.
    
    This seems like a really hacky workaround to something that should
    ideally be fixed at a different level, so I'm hoping I can back this out
    again later.

--- add_full_P39.js
+++ add_full_P39.js
@@ -2,9 +2,9 @@ module.exports = (id, startdate, enddate, replaces, replacedby) => {
   const qualifiers = { }
 
   // Seems like there should be a better way of filtering these...
-  if (startdate && startdate != "''")  qualifiers['P580']  = startdate
-  if (enddate && enddate != "''")    qualifiers['P582']  = enddate
-  if (replaces && replaces != "''")   qualifiers['P1365'] = replaces
+  if (startdate && startdate != "''")   qualifiers['P580']  = startdate
+  if (enddate && enddate != "''")       qualifiers['P582']  = enddate
+  if (replaces && replaces != "''")     qualifiers['P1365'] = replaces
   if (replacedby && replacedby != "''") qualifiers['P1366'] = replacedby
 
   if (startdate && enddate && (startdate > enddate)) throw new Error(`Invalid dates: ${startdate} / ${enddate}`)
